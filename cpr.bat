086265b978cd4e3cde0dcb05eea263c47c392514
tnishi <sgm00353@nifty.ne.jp>
2016-11-28 23:14:34 +0900

    VS2013対応へ

--- cpr.bat
+++ cpr.bat
@@ -1,3 +1,3 @@
-copy DCHook\Win32\Release\DCHook.dll DCHookTest\DKPUHK.DLL
+copy DCHook\Release\DCHook.dll DCHookTest\DKPUHK.DLL
 copy DCHook\x64\Release\DCHook.dll DCHookTest\DKPUHK64.DLL
-copy DCHook64\dchk64\x64\Release\dchk64.exe DCHookTest\DKPUHK64.exe
+rem copy DCHook64\dchk64\x64\Release\dchk64.exe DCHookTest\DKPUHK64.exe
